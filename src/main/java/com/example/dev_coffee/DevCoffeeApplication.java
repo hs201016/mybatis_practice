905de7703e06875b93cbcc2945b5c65c0ab9d3cd
류희수 <hs201016@gmail.com>
2024-09-06 16:05:51 +0900

    Ignore File

---
+++ src/main/java/com/example/dev_coffee/DevCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev_coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCoffeeApplication.class, args);
+	}
+
+}
